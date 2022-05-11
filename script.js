1a62accecab35776939ded4dd884c161c04a7b33
Salman Ahmed <salmanahmedx@hotmail.com>
2022-05-11 12:48:23 +0600

    text and button animation

---
+++ script.js
@@ -0,0 +1,90 @@
+const tl = gsap.timeline({
+    defaults: {
+        duration: 0.75,
+        ease: "power3.out"
+    }
+});
+
+tl.fromTo(
+    ".hero-img", {
+    scale: 1.3,
+    borderRadius: "0rem"
+}, {
+    scale: 1,
+    borderRadius: "2rem",
+    delay: 0.35,
+    duration: 2.5,
+    ease: "elastic.out(1.5,1)",
+}
+);
+
+tl.fromTo(
+    ".cta-1", {
+    x: "100%",
+    opacity: 0.5,
+}, {
+    x: 0,
+    opacity: 1,
+},
+    "<20%"
+);
+
+tl.fromTo(
+    ".cta-3", {
+    x: "-100%",
+    opacity: 0.5,
+}, {
+    x: 0,
+    opacity: 1,
+},
+    "<20%"
+);
+
+tl.fromTo(
+    ".cta-2", {
+    y: "100%",
+    opacity: 0,
+}, {
+    y: 0,
+    opacity: 1,
+},
+    "<20%"
+);
+
+tl.fromTo(
+    ".cta-4", {
+    x: "100%",
+    opacity: 0.5,
+}, {
+    x: 0,
+    opacity: 1,
+},
+    "<20%"
+);
+
+tl.fromTo(
+    ".cta-5", {
+    y: "100%",
+    opacity: 0,
+}, {
+    y: 0,
+    opacity: 1,
+},
+    "<20%"
+);
+
+tl.fromTo(
+    ".cta-6", {
+    x: "-100%",
+    opacity: 0.5,
+}, {
+    x: 0,
+    opacity: 1,
+},
+    "<20%"
+);
+
+tl.fromTo(".cta-btn", {
+    y: 20, opacity: 0
+}, { y: 0, opacity: 1 }, "<"
+)

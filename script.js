d442f322ed0801803d2464cb123ea69ae7e05680
Salman Ahmed <salmanahmedx@hotmail.com>
2022-05-11 13:05:52 +0600

    logo animation using stagger and GSAP

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+//landing page text animation
+
 const tl = gsap.timeline({
     defaults: {
         duration: 0.75,
@@ -88,3 +90,17 @@ tl.fromTo(".cta-btn", {
     y: 20, opacity: 0
 }, { y: 0, opacity: 1 }, "<"
 )
+
+// logo animation - Split text alternative
+
+const logo = document.querySelector(".logo");
+const letters = logo.textContent.split("");
+logo.textContent = "";
+
+letters.forEach(letter => {
+    logo.innerHTML += `<span class="letter">${letter}</span>`;
+})
+
+
+gsap.set(".letter", { display: "inline-block" });
+gsap.fromTo(".letter", { y: 100 }, { y: 0, delay: 2, stagger: 0.05, ease: 'back.out(2)' })
